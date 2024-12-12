821cd1655793dd86ffecca0fd6607cc2e20f2450
Mohamed Dr <hinatashoyo556@gmail.com>
2024-12-13 00:41:45 +0100

    ;

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import TodoGetCreate, TodoGetUpdateDelete
-
-urlpatterns = [
-    path('',TodoGetCreate.as_view()), 
-    path('<int:pk>', TodoGetUpdateDelete.as_view())
-]
